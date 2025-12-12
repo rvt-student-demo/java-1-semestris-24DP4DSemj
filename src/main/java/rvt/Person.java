6a99551c7dbc2fdc0662ab698faf46ff5b964c0b
24DP4DSemj <A240092DS@rvt.lv>
2025-12-12 09:13:13 +0200

    Person and subclasses (1)

---
+++ src/main/java/rvt/Person.java
@@ -0,0 +1,26 @@
+package rvt;
+
+public class Person {
+
+    private String name;
+    private String address;
+
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+    // other constructors and methods
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + "\n  " + this.address;
+    }
+}
